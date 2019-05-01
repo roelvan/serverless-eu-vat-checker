d841ff4e384a83bba9bde296498edbc8f230921e
Roel Vaneyghen <roel.vaneyghen@gmail.com>
2019-05-01 09:39:40 +0700

    add error code in case the vat service is not respinding

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ module.exports = async (req, res) => {
   if (vat && vat.length > 10) {
     // remove spaces and other weird chars
     const vatNr = vat.replace(/[. ,:-]+/g, '');
-    const response = await fetch(
-      `https://controleerbtwnummer.eu/api/validate/${vat}.json`
-    );
-    res.end(JSON.stringify(await response.json()));
+    try {
+      const response = await fetch(
+        `https://controleerbtwnummer.eu/api/validate/${vat}.json`
+      );
+      res.end(JSON.stringify(await response.json()));
+    } catch (error) {
+      res.statusCode = 500;
+      res.end('Service offline.');
+    }
   } else {
     res.end(JSON.stringify({ valid: false }));
   }
