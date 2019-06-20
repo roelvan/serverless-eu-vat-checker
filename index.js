7c243d78363b292433e1170c0be40e1427b782ac
Roel Vaneyghen <roel.vaneyghen@gmail.com>
2019-06-20 09:35:49 +0700

    use new node helpers

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const fetch = require('isomorphic-fetch');
 
 module.exports = async (req, res) => {
   // For CORS
-  if (req.method === 'OPTIONS') {
-    return res.end('ok');
-  }
-  const { query } = parse(req.url, true);
-  const { vat } = query;
+  if (req.method === 'OPTIONS') return res.json('ok');
+
+  const { vat } = req.query;
+  
   if (vat && vat.length > 10) {
     // remove spaces and other weird chars
     const vatNr = vat.replace(/[. ,:-]+/g, '');
@@ -15,12 +14,12 @@ module.exports = async (req, res) => {
       const response = await fetch(
         `https://controleerbtwnummer.eu/api/validate/${vat}.json`
       );
-      res.end(JSON.stringify(await response.json()));
+      res.json(await response.json());
     } catch (error) {
-      res.statusCode = 500;
-      res.end('Service offline.');
+      res.status(500);
+      res.json('Service offline.');
     }
   } else {
-    res.end(JSON.stringify({ valid: false }));
+    res.json({ valid: false });
   }
 };
