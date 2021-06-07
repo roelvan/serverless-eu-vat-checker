19738b20a2973112d2a2fd009eee19dbc1f4ea92
Roel Vaneyghen <roel.vaneyghen@gmail.com>
2021-06-07 21:49:16 +1200

    add jsvat checker

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const { parse } = require("url");
-const fetch = require("isomorphic-unfetch");
-const cors = require("./cors")();
-const { handleErrors } = require("./errors");
+const { parse } = require('url');
+const { checkVAT, countries } = require('jsvat');
+const fetch = require('isomorphic-unfetch');
+const cors = require('./cors')();
+const { handleErrors, ForbiddenError, NotFoundError } = require('./errors');
 
 const handler = async (req, res) => {
-  const { vat } = req.query;
+  if (req.method !== 'GET') throw new ForbiddenError();
 
-  if (!vat || vat.length < 10) return res.json({ valid: false });
-  // remove spaces and other weird chars
-  const vatNr = vat.replace(/[. ,:-]+/g, "");
-  const response = await fetch(
-    `https://controleerbtwnummer.eu/api/validate/${vatNr}.json`
-  );
+  const { vatId } = req.query;
 
-  return res.json(await response.json());
+  const { isValid, value: vatValue, country } = checkVAT(vatId, countries);
+  if (!isValid) throw new NotFoundError();
+
+  const response = await fetch(`https://controleerbtwnummer.eu/api/validate/${vatValue}.json`);
+  if (response.status !== 200) {
+    return res.json({
+      valid: true,
+      countryCode: country?.isoCode.short || 'BE',
+      vatNumber: vatValue.replace('BE', ''),
+    });
+  }
+
+  const data = await response.json();
+
+  res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
+  return res.json(data);
 };
 
 module.exports = handleErrors(cors(handler));
